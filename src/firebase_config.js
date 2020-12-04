40c4e02e3030271d463465dbf62b60457611d3ed
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-04 20:27:34 +0530

    db config

---
+++ src/firebase_config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmru40x5VKcTPrQ1-lsS3PrQI77LNA9f4",
+  authDomain: "react-fire-todo-9325d.firebaseapp.com",
+  projectId: "react-fire-todo-9325d",
+  storageBucket: "react-fire-todo-9325d.appspot.com",
+  messagingSenderId: "368961429744",
+  appId: "1:368961429744:web:0e2fffae601737ba60e235",
+  measurementId: "G-ZTHYWEK9XX",
+};
+
+firebase.initializeApp(firebaseConfig);
+const { db } = firebase.firestore();
+export { db };
