a34d8533fe4dc01a2949cd5d5e2d42388770fc72
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-05 00:13:55 +0530

    list and buttons are styled with flex

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,6 +1,18 @@
+import { Button, ListItem, ListItemText } from "@material-ui/core";
 import React from "react";
 
 function TodoListItem({ todo, inprogress, id }) {
-  return <div>{todo}</div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <ListItem>
+        <ListItemText
+          primary={todo}
+          secondary={inprogress ? "In Progress" : "Completed"}
+        ></ListItemText>
+      </ListItem>
+      <Button>Done</Button>
+      <Button>X</Button>
+    </div>
+  );
 }
 export default TodoListItem;
