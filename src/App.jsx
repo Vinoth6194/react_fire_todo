6971bcfc1e338b5bbd517e838b66cd0343eb79f4
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-04 20:20:55 +0530

    Textfield

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
     <div className="App">
       <h1>Vinothkumar's Todo App 🔥 🔥 </h1>
+      <TextField
+        id="standard-basic"
+        label="Vin's Todo"
+        className="todo_field"
+      />
     </div>
   );
 }
